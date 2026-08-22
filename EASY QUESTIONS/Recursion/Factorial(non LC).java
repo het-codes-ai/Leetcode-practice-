a2ad069cdae9301df96080508df6c16f4429fa41
HET MODI <modijignisha12@gmail.com>
2026-08-22 16:24:18 +0530

    Create Factorial(non LC).java

---
+++ EASY QUESTIONS/Recursion/Factorial(non LC).java
@@ -0,0 +1,12 @@
+public class Main {
+	public static int  fact(int n){
+		if(n==1 || n==0){
+			return 1;
+			}	
+		return n*fact(n-1);
+		}
+	public static void main(String[] args) {
+		fact(5);
+		System.out.println(fact(6));
+	}
+}
