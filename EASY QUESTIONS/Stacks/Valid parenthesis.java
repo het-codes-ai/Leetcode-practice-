733e98c65e0769c716fa52a7d73cc24e20d9a519
HET MODI <modijignisha12@gmail.com>
2026-09-01 16:44:40 +0530

    Update Valid parenthesis.java

--- EASY QUESTIONS/Stacks/Valid parenthesis.java
+++ EASY QUESTIONS/Stacks/Valid parenthesis.java
@@ -1,30 +1,29 @@
+import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> a=new Stack<>();
+        Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
-            //opening braces
-            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{' ){
-                a.push(s.charAt(i));
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                st.push(s.charAt(i));
             }
-            //closing braces 
             else{
-                if(a.isEmpty()){
+                if(st.isEmpty()){
                     return false;
                 }
                 else{
-                    char top=a.pop();
-                    if(s.charAt(i)==')' && top!='('){
-                        return false;
-                    }
-                     if(s.charAt(i)=='}' && top!='{'){
-                        return false;
-                    }
-                     if(s.charAt(i)==']' && top!='['){
-                        return false;
-                    }
-                }
+            }
+            if( s.charAt(i)==')' && st.peek()!='('){
+                return false;
+            }
+            if( s.charAt(i)==']' && st.peek()!='['){
+                return false;
+            }
+            if( s.charAt(i)=='}' && st.peek()!='{'){
+                return false;
+            }
+            st.pop();
             }
         }
-        return a.isEmpty();
+        return st.isEmpty();
     }
 }
